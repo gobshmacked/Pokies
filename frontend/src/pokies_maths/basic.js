cba179030d438b4b2ff522c049434187a8854638
James White <james.dafydd.white1@gmail.com>
2024-09-17 17:59:12 +1000

    implemented base random pokie machine class

--- frontend/src/pokies_maths/basic.js
+++ frontend/src/pokies_maths/basic.js
@@ -1,37 +1,70 @@
 // ME messing around trying to find good way to make number sequence
 
-// Draft for iconography
-// 5 letters, 5 symboles, 2 numbers
-// letters are filler and create a line with at least 2 pictures assuming all letters are the same
-// Win on lines and diagonals, never only vertial although a middle vertical line and diagonals are good
-// 1 cool looking filler symbol that appears often and bridges the gap between other letters or symbols
-// vertical based symbol that acts as a filler for rows of 3
-// Hyper rare jackpot symbol
-// More common symbol for advances features
-// 3 in a row enough for a small win, including with vertical filler
-
-// Takes an array of integers where weightings[i] is how much more often i should appear than other values. E.g w[i] = 1 and w[j] = 10. w[j] should appear 10 times more often
+// Takes an array of integers where probabilitySeries[i] is how much more often i should appear than other values. E.g w[i] = 1 and w[j] = 10. w[j] should appear 10 times more often
 // values should all be integers 1 or greater
 // Returns a single random weighted value
 
-const weightedValue = (weightings) => {
-	if (weightings.length === 0) return -1
+const weightedValue = (probabilitySeries) => {
+	if (probabilitySeries.length === 0) return -1
 
 	let sum = 0 
-	for (let i = 0; i < weightings.length; i++) {
-		sum += weightings[i]
+	for (let i = 0; i < probabilitySeries.length; i++) {
+		sum += probabilitySeries[i]
 	}
 	let tempsum = 0
 	let rand = Math.random()
-	// determine which integer best aligns with rand when considering weightings
-	for (let i = 0; i < weightings.length; i++) {
-		if (rand < tempsum + (1.0 * weightings[i] / sum)) {
+
+	// determine which integer best aligns with rand when considering probabilitySeries
+	// done by each weighting[i] / length which gives the probability of it occuring as a decimal -> decimal used in cumulative probability total 
+	for (let i = 0; i < probabilitySeries.length; i++) {
+		if (rand < tempsum + (1.0 * probabilitySeries[i] / sum)) {
 			return i
 		}
-		tempsum += 1.0 * weightings[i] / sum
+		tempsum += 1.0 * probabilitySeries[i] / sum
 	}
 }
 
-testArray = [100,100,100,100,100,50,50,50,20,10,1]
+// This object is the basic code required for a slot machine that operates solely on random numbers and a sequence of realtive probabilities for each option
+class BaseMachine {
+	constructor(probabilitySeries) {
+		this.probabilitySeries = probabilitySeries
+	}
 
-console.log(weightedValue(testArray))
+	generate() {
+		let ans = []
+		for (let i = 0; i < 15; i++) {
+			ans.push(weightedValue(this.probabilitySeries))
+		}
+		return ans
+	}
+
+	generatePrint() {
+		let ans = this.generate()
+		console.log('Results')
+		console.log(ans.slice(0, 5))
+		console.log(ans.slice(5, 10))
+		console.log(ans.slice(10, 15))
+	}
+}
+
+const testWeightedValue = (calls, probabilitySeries) => {
+	let ans = []
+	for (let i = 0; i < probabilitySeries.length; i++) {
+		ans.push(0)
+	}
+	for (let i = 0; i < calls; i++) {
+		ans[weightedValue(probabilitySeries)] += 1
+	}
+	for (let i = 0; i < probabilitySeries.length; i++) {
+		console.log(`${i} appears ${ans[i]}`)
+	}
+}
+
+testArray = [100,100,100,100,100,100,100,50,50,50,50,50,20,10,1]
+
+
+// OUTPUTTING TO CONSOLE BELOW HERE
+// testWeightedValue(1000000, testArray)
+// console.log(weightedValue(testArray))
+const machine = new BaseMachine(testArray)
+machine.generatePrint()
