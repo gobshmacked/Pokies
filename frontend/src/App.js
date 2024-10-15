aac657f9f93b35b24192e2fb01978a56e890307b
James White <james.dafydd.white1@gmail.com>
2024-10-16 00:47:28 +1100

    website template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Screen } from './react_components/Screen.jsx'
 
 function App() {
+	document.title = "SuperNova"
   return (
     <div className="App">
-      <header className="App-header">
-				{}
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Screen/>
     </div>
   );
 }
---
+++ frontend/src/react_components/AnswerGrid.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { styled } from '@mui/material'
+
+export function AnswerGrid() {
+
+}
+
+const PokerMachineGrid = styled('div')({
+	display: 'grid',
+	
+})
