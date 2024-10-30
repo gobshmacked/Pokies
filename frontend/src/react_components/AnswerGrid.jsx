55eecb96aabb349edc1fe1e3ba25ab9b9f66234c
James White <james.dafydd.white1@gmail.com>
2024-10-30 11:49:23 +1100

    updated symbols to sync with game state

--- frontend/src/react_components/AnswerGrid.jsx
+++ frontend/src/react_components/AnswerGrid.jsx
@@ -4,11 +4,22 @@ import loadingIcon from './assets/pokie_images/loading.png';
 import image0a from './assets/pokie_images/0.png';
 import image0b from './assets/pokie_images/0b.png';
 import image0c from './assets/pokie_images/0c.png';
-import image1 from './assets/pokie_images/1.png';
-import image2 from './assets/pokie_images/2.png';
-import image3 from './assets/pokie_images/3.png';
-import image4 from './assets/pokie_images/4.png';
-import image5 from './assets/pokie_images/5.png';
+import image1a from './assets/pokie_images/1a.png';
+import image1b from './assets/pokie_images/1b.png';
+import image1c from './assets/pokie_images/1c.png';
+import image2a from './assets/pokie_images/2a.png';
+import image2b from './assets/pokie_images/2b.png';
+import image2c from './assets/pokie_images/2c.png';
+import image3a from './assets/pokie_images/3a.png';
+import image3b from './assets/pokie_images/3b.png';
+import image3c from './assets/pokie_images/3c.png';
+import image4a from './assets/pokie_images/4a.png';
+import image4b from './assets/pokie_images/4b.png';
+import image4c from './assets/pokie_images/4c.png';
+import image5a from './assets/pokie_images/5a.png';
+import image5b from './assets/pokie_images/5b.png';
+import image5c from './assets/pokie_images/5c.png';
+
 import image6 from './assets/pokie_images/6.png';
 import image7 from './assets/pokie_images/7.png';
 import image8 from './assets/pokie_images/8.png';
@@ -19,13 +30,28 @@ import image12 from './assets/pokie_images/12.png';
 
 
 export function AnswerGrid(props) {
-  const images = [image0a, image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12];
+  const images = [image0a, image1a, image2a, image3a, image4a, image5a, image6, image7, image8, image9, image10, image11, image12];
 	if (props.gameState === 'green') {
 		images[0] = image0a
+		images[1] = image1a
+		images[2] = image2a
+		images[3] = image3a
+		images[4] = image4a
+		images[5] = image5a
 	} else if (props.gameState === 'yellow') {
 		images[0] = image0b
+		images[1] = image1b
+		images[2] = image2b
+		images[3] = image3b
+		images[4] = image4b
+		images[5] = image5b
 	} else if (props.gameState === 'red') {
 		images[0] = image0c
+		images[1] = image1c
+		images[2] = image2c
+		images[3] = image3c
+		images[4] = image4c
+		images[5] = image5c
 	}
 
   return (
